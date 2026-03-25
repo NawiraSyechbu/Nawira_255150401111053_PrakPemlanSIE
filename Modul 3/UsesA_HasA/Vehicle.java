e3be9d389ada3fb8cf3b3b5bca9f1132431f8ca1
NawiraSyechbu <nawirabsa06@gmail.com>
2026-03-25 16:52:33 +0700

    Create Vehicle.java

---
+++ Modul 3/UsesA_HasA/Vehicle.java
@@ -0,0 +1,15 @@
+package UsesA_HasA;
+
+public class Vehicle {
+    Vehicle() {
+        System.out.println("Vehicle is created");
+    }
+
+    public void drive() {
+        System.out.println("Vehicle is driving");
+    }
+
+    public void stop() {
+        System.out.println("Vehicle is stopping");
+    }
+}
