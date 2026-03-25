8832dbf5c76eca178ce26511cf01e3ba72bbb3fe
NawiraSyechbu <nawirabsa06@gmail.com>
2026-03-25 16:55:42 +0700

    Create DrivingApp.java

---
+++ Modul 3/UsesA_HasA/DrivingApp.java
@@ -0,0 +1,9 @@
+package UsesA_HasA;
+
+public class DrivingApp {
+    public static void main(String[] args) {
+      //Instansiasi objek class Driver
+        Driver miki = new Driver();
+        miki.driving();
+    }
+}
