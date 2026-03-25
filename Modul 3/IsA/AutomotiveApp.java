e51a8491341a7f739ae4b571ea01ae7f1998f6bd
NawiraSyechbu <nawirabsa06@gmail.com>
2026-03-25 16:51:38 +0700

    Create AutomotiveApp.java

---
+++ Modul 3/IsA/AutomotiveApp.java
@@ -0,0 +1,24 @@
+public class AutomotiveApp {
+    public static void main(String[] args) {
+
+      //Instansiasi objek
+        Vehicle vehicle = new Vehicle();
+        Car car = new Car();
+        Sedan sedan = new Sedan();
+
+        vehicle.drive();
+        car.drive();
+        sedan.drive();
+
+        System.out.println("-----------------");
+        car.drift();
+        sedan.drift();
+
+        System.out.println("-----------------");
+        car.honk();
+        sedan.honk();
+        
+        System.out.println("-----------------");
+        sedan.getNumWheels();
+    }
+}
