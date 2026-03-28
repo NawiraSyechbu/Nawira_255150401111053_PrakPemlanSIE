ecee9c7d4b23b7c63177bef9ec8c0909893e9bf9
NawiraSyechbu <nawirabsa06@gmail.com>
2026-03-28 20:37:56 +0700

    Create TiketBus.java

---
+++ Modul 4/Override/TiketBus.java
@@ -0,0 +1,30 @@
+package Override;
+
+public class TiketBus extends Tiket{
+    @Override
+    public void pesan() {
+        this.tujuan = "Bandung";
+    }
+
+    public void pesan(String n){
+        this.namaPenumpang = n;
+    }
+
+    @Override
+    public void pesan(String n, String t) {
+        if (t.equals("Makassar")) {
+            this.tujuan = "Makassar";
+            System.out.println("Maaf " + n + ", tiket tidak dapat dipesan.");
+        }
+    }
+
+    @Override
+    public void tampil() {
+        if (!this.tujuan.equals("Makassar")) {
+            super.tampil();
+        } else {
+            System.out.println("Tiket tujuan Makassar tidak tersedia.");
+            System.out.println("--------------------");
+        }
+    }
+}
