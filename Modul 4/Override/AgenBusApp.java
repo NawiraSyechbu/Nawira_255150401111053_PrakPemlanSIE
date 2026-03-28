4f0c9823318be6beb0c8192688dd4bd773c11f25
NawiraSyechbu <nawirabsa06@gmail.com>
2026-03-28 20:38:27 +0700

    Create AgenBusApp.java

---
+++ Modul 4/Override/AgenBusApp.java
@@ -0,0 +1,44 @@
+package Override;
+
+public class AgenBusApp {
+    public static void main(String[] args) {
+        Tiket t1 = new Tiket();
+        t1.pesan();
+        t1.tampil();
+
+        TiketBus t2 = new TiketBus();
+        t2.pesan();
+        t2.tampil();
+
+        TiketBus t3 = new TiketBus();
+        t3.pesan("Kasino", "Bandung", 650, 450000);
+        t3.tampil();
+
+        t1.pesan("Indro", "Makassar");
+        t1.tampil();
+
+        t2.pesan("Dono", "Makassar");
+        t2.tampil();
+
+        TiketBus t4 = new TiketBus();
+        t4.pesan("Sinta");
+        t4.tampil();
+
+        TiketBus t5 = new TiketBus() {
+            @Override
+            public void tampil(){
+                System.out.println("----------------------------------------------------");
+                System.out.println("|                      TIKETMU                     |");
+                System.out.println("----------------------------------------------------\n");
+                System.out.println(">> Penumpang    : " +this.namaPenumpang);
+                System.out.println(">> Tujuan       : " +this.tujuan);
+                System.out.println(">> Harga        : Rp. " +this.hargaTiket);
+                System.out.println("\n------- Terima Kasih telah Mempercayai Kami -------\n");
+            }
+        };
+        t5.pesan("Zara", "Yogyakarta", 468, 350000);
+        t5.tampil();
+
+        
+    }
+}
